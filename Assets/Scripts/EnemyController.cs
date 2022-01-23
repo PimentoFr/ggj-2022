dc97909ad8fe83ef6f5baaf7f52da550d5acfb56
Klaarkh <klaarkh.twitch@gmail.com>
2022-01-23 08:31:48 +0100

    Ajout du PNJ qui se balade et qui fait monter le stress( avec portee, timeout)

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    BoxCollider2D hitbox;
+    SpriteRenderer enemy;
+
+    
+    public int direction = 1;
+    public float speed = 3.0f;
+    public float timeChange = 3.0f;
+    public float timeOut = 3.0f;
+    public float portee = 10.0f;
+
+    private float movingTime;
+    private Vector2 position;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        enemy = GetComponent<SpriteRenderer>();
+        hitbox = GetComponent<BoxCollider2D>();
+        movingTime = timeChange;
+        hitbox.size = new Vector2(portee, 8.0f);
+    }
+
+    
+    void FixedUpdate()
+    {
+        
+        timeManager();
+    }
+
+    void move()
+    {
+        position = rb.position;
+
+        position.x += speed * direction * Time.deltaTime;
+
+       
+        anim.SetBool("isMoving", true);
+        enemy.flipX = (direction != 1);
+
+        if(direction==1)
+            hitbox.offset = new Vector2(hitbox.size.x / 2, 0); 
+        
+        else
+            hitbox.offset = new Vector2(-hitbox.size.x / 2, 0);
+
+        rb.MovePosition(position);
+
+    }
+
+    void timeManager()
+    {
+        if (movingTime < 0)
+        {
+            movingTime = timeChange;
+            direction = -direction;
+            hitbox.enabled = true;
+            anim.SetBool("hasFlicked", false);
+        }
+
+        else if(hitbox.enabled)
+        {
+            move();
+            movingTime -=  Time.deltaTime;
+        }
+
+        else
+        {
+            movingTime -= Time.deltaTime;
+        }
+            
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerMoves player = collision.gameObject.GetComponent<PlayerMoves>();
+
+        if(player != null && player.isTricking)
+        {
+            player.changeStress(10);
+            hitbox.enabled = false;
+            movingTime = timeOut;
+            anim.SetBool("hasFlicked", true);
+        }
+        
+    }
+}
