6b8a953f4db9df29d1b6ddb0f9001515feef79dc
Pimento <pimento.stream@gmail.com>
2022-01-23 09:38:19 +0100

    add stress bar on the screen and synchronize with player info

--- Assets/Scripts/StressLevel.cs
+++ Assets/Scripts/StressLevel.cs
@@ -6,20 +6,18 @@ using UnityEngine.UI;
 public class StressLevel : MonoBehaviour
 {
     public Image mask;
-
-    [Range(0f, 100f)]
-    public float stressPercent = 0f;
-
     float originalSize;
+    PlayerInfo playerInfo;
 
     void Start()
     {
+        playerInfo = GameObject.FindWithTag("Player").GetComponent<PlayerInfo>();
         originalSize = mask.rectTransform.rect.width;
     }
 
     void FixedUpdate()
     {
-        stressPercent = Mathf.Clamp(stressPercent, 0f, 100f);
+        float stressPercent = Mathf.Clamp(playerInfo.GetStress(), 0f, 100f);
         mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * stressPercent / 100f);
     }
 }
