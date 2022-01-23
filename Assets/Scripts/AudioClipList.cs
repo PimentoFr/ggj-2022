4302add1a48a1e705311ec5bfee9fa3532532693
Pimento <pimento.stream@gmail.com>
2022-01-23 18:21:02 +0100

    list all audio clip

---
+++ Assets/Scripts/AudioClipList.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioClipList : MonoBehaviour
+{
+    public AudioClip ambiancePunk;
+    public AudioClip ambianceLoop;
+
+    public AudioClip posterClean;
+    public AudioClip posterPlace;
+    public AudioClip posterChaos;
+
+    public AudioClip agraferUse;
+    public AudioClip agraferCollect;
+    public AudioClip agraferPlayClips;
+
+    public AudioClip penChoose;
+    public AudioClip penMoisture;
+    public AudioClip penPick;
+    public AudioClip penSign;
+    public AudioClip penThrow;
+
+    public AudioClip chairRemove;
+    public AudioClip chairMove;
+    public AudioClip chairFartingBag;
+
+    public AudioClip fileOrder;
+    public AudioClip fileBill;
+    public AudioClip fillChaos;
+
+    public AudioClip coffeeMake;
+    public AudioClip coffeePutMug;
+    public AudioClip coffeeCleanMug;
+    public AudioClip cofferFillWater;
+    public AudioClip coffeePoison;
+
+    public AudioClip computerSwitchOn;
+    public AudioClip computerSwitchOff;
+    public AudioClip computerUpdate;
+    public AudioClip computerStopYoutube;
+    public AudioClip computerRunYoutube;
+
+    public AudioClip copierDropPhotos;
+    public AudioClip copierFill;
+    public AudioClip copierTake;
+    public AudioClip copierAss;
+
+    public AudioClip paperOrder;
+    public AudioClip paperUnfold;
+    public AudioClip paperPlane;
+
+    public AudioClip plantWater;
+    public AudioClip plantPlace;
+    public AudioClip plantFall;
+
+    public AudioClip phoneBullshit;
+    public AudioClip phoneType;
+    public AudioClip phoneConvince;
+    public AudioClip phoneApologize;
+    public AudioClip phonePrank;
+
+    public AudioClip DefaultCorrect;
+    public AudioClip DefaultFailed;
+    public AudioClip DefaultSwipe;
+}
