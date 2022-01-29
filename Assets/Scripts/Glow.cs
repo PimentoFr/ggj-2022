f63d6d8d92664ba5ae5ddc6200c143fe23e65b1a
Unknown <zmon@live.fr>
2022-01-29 20:21:55 +0100

    Space Bar & Glow blue Ascenseur

--- Assets/Scripts/Glow.cs
+++ Assets/Scripts/Glow.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Glow : MonoBehaviour
 {
     public Interactable m_callback;
     private Collider2D m_collider;
     public bool glowEnabledEverytime = false;
+    public bool isElevator = false;
 
     GameObject glowingGO;
     GameObject interactibleGO;
+    GameObject spacebarGO;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +21,10 @@ public class Glow : MonoBehaviour
         m_callback = GetComponent<Interactable>();
         glowingGO = transform.GetChild(0).gameObject;
         interactibleGO = transform.parent.gameObject;
+        
+        spacebarGO = GameObject.FindGameObjectWithTag("SpaceBar");
     }
-    
+
     // Update is called once per frame
     void Update()
     {
@@ -28,25 +33,46 @@ public class Glow : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        bool isSpacebar = true;
         GameObject obj = collision.gameObject;
         if (obj.tag == "Player")
         {
-            if (obj.GetComponent<PlayerInfo>().GetIsTricking() && 
-                (glowEnabledEverytime || interactibleGO.GetComponent<StateInteractable>().GetState() != StateInteractableObject.OUT_OF_SERVICE))
+            if (isElevator)
             {
-                glowingGO.GetComponent<SpriteRenderer>().color = new Color(1f,0f,0f,0.3f);
+                glowingGO.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 1f, 0.3f);
                 glowingGO.SetActive(true);
+                GameObject.FindGameObjectWithTag("UI_TaskLabels").GetComponent<TaskLabel>().ShowTaskLabel("Take lift", false, false);
             }
-            else if(!obj.GetComponent<PlayerInfo>().GetIsTricking() &&
-                    (glowEnabledEverytime || interactibleGO.GetComponent<StateInteractable>().GetState() != StateInteractableObject.FIXED))
+            else
             {
-                glowingGO.GetComponent<SpriteRenderer>().color = new Color(0f, 1f, 0f, 0.3f);
-                glowingGO.SetActive(true);
+
+                if (obj.GetComponent<PlayerInfo>().GetIsTricking() &&
+                    (glowEnabledEverytime || interactibleGO.GetComponent<StateInteractable>().GetState() != StateInteractableObject.OUT_OF_SERVICE))
+                {
+                    glowingGO.GetComponent<SpriteRenderer>().color = new Color(1f, 0f, 0f, 0.3f);
+                    glowingGO.SetActive(true);
+                }
+                else if (!obj.GetComponent<PlayerInfo>().GetIsTricking() &&
+                        (glowEnabledEverytime || interactibleGO.GetComponent<StateInteractable>().GetState() != StateInteractableObject.FIXED))
+                {
+                    glowingGO.GetComponent<SpriteRenderer>().color = new Color(0f, 1f, 0f, 0.3f);
+                    glowingGO.SetActive(true);
+                }
+                else
+                {
+                    glowingGO.SetActive(false);
+                    isSpacebar = false;
+                }
             }
 
-            else {
-                glowingGO.SetActive(false);
-            }            
+            if ((obj.GetComponent<PlayerInfo>().IsActionDoing()) || !isSpacebar)
+            {
+                spacebarGO.GetComponent<Image>().enabled = false;
+            }
+            else
+            {
+                spacebarGO.GetComponent<Image>().enabled = true;
+            }
         }
     }
 
@@ -56,6 +82,11 @@ public class Glow : MonoBehaviour
         if (obj.tag == "Player")
         {
             glowingGO.SetActive(false);
+            spacebarGO.GetComponent<Image>().enabled = false;
+            if (isElevator)
+            {
+                GameObject.FindGameObjectWithTag("UI_TaskLabels").GetComponent<TaskLabel>().HideTaskLabel();
+            }
         }
     }
 }
